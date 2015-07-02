e88dcd2ca71a240b61eb163ddd996871d2631ddb
DJ Mosca <djmosc@gmail.com>
2015-07-02 14:57:24 +0100

    Fixed car leasing prices form

--- inc/modules/advert.php
+++ inc/modules/advert.php
@@ -2,4 +2,4 @@
 $class = array('advert');
 $class[] = ( !empty($options['responsive']) ) ? 'responsive' : ''; 
 ?>
-<div id="<?php echo $placement_id; ?>" class="<?php echo implode(' ', $class); ?>" data-options="<?php echo ( !empty($options) ) ? esc_js(json_encode($options, JSON_NUMERIC_CHECK)) : ''; ?>" data-placement-id="<?php echo $placement_id; ?>"></div>
+<div id="<?php echo $placement_id; ?>" class="<?php echo implode(' ', $class); ?>" data-keywords="<?php echo ( !empty($options['params']['key']) ) ? esc_attr($options['params']['key']) : ''; ?>" data-options="<?php echo ( !empty($options) ) ? esc_js(json_encode($options, JSON_NUMERIC_CHECK)) : ''; ?>" data-placement-id="<?php echo $placement_id; ?>"></div>
